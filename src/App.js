96e5fe0e5e9e5431e2fa6cbe5af703c1a37d1b48
MuhammadSirajAbbasi <64827860+Siraj454@users.noreply.github.com>
2020-06-13 05:40:20 +0500

    updated display from app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div>
-   Hello world! from <br></br> Muhammad Siraj Abbasi abcd
+   Hello world! from <br></br> Muhammad Siraj Abbasi 
+   <br></br>thanks <b>PIAIC</b> whole team
     </div>
   );
 }
